f5ec4a60aec5ac69932b212e9ad2562946839f64
Icejewel0319 <icejewel0319@gmail.com>
2020-11-21 10:16:21 -0800

    initial

---
+++ calculation/boxoffice_result.py
@@ -0,0 +1,57 @@
+import mysql.connector
+import requests
+import re
+import json
+
+con = mysql.connector.connect(host='127.0.0.1',user='dsci551',password='dsci551',database='movie')
+cur = con.cursor()
+
+title = input('Enter movie title:')
+
+def remove_special_symbol(word):
+    regex = re.compile('[^0-9]')
+    return regex.sub('', word)
+
+query = "SELECT Domestic_percent,Foreign_percent,Domestic_gross,Foreign_gross,Domestic_opening FROM movie.boxoffice_table where Title ='"+ title +"'"
+cur.execute(query)
+result_dict = {}
+for row in cur:
+    print(row)
+    if row[0] != "-":
+        d_percent = float(row[0].replace("%",""))
+    elif row[0] == "-":
+        d_percent = 0
+    if row[1] != "-":
+        f_percent = float(row[1].replace("%",""))
+    elif row[1] == "-":
+        f_percent = 0
+
+    d_gross = int(remove_special_symbol(row[2]))
+    f_gross = int(remove_special_symbol(row[3]))
+    if row[4] !="-":
+        d_opening = int(remove_special_symbol(row[4]))
+        opening_percent = float("{0:.2f}".format((d_opening/d_gross)*100))
+        rest_percent = 100 - opening_percent
+        result_dict['Opening Percent'] = opening_percent
+        result_dict['Rest Percent'] = rest_percent
+    elif row[4] == "-":
+        d_opening = 0
+        result_dict['Opening Percent'] = 0
+        result_dict['Rest Percent'] = 100
+
+
+
+    result_dict['Domestic Percent'] = d_percent
+    result_dict['Foreign Percent'] = f_percent
+    result_dict['Domestic Gross(in dollars)'] = d_gross
+    result_dict['Foreign Gross(in dollars)'] = f_gross
+    result_dict['Domestic Opening Gross(in dollars)'] = d_opening
+
+
+print(result_dict)
+
+for i in result_dict:
+    dict1 = {}
+    dict1[i] = result_dict[i]
+    print(dict1)
+    requests.patch(url="https://dsci551-a1e31.firebaseio.com/Movie_info_result/BoxofficeAnalysis/.json", json=dict1)
---
+++ calculation/webpage2_movie.py
@@ -0,0 +1,53 @@
+import requests
+import json
+import sys
+from collections import defaultdict
+
+#main function
+if __name__ == '__main__':
+    expected_movie = str(sys.argv[1])
+    data_movie = requests.get('https://dsci551-a1e31.firebaseio.com/Movie_info.json')
+    data_movie = json.loads(data_movie.text)
+    result = {}
+
+    for i in data_movie:
+        if  expected_movie == str(data_movie[i]['Title']):
+            actors = data_movie[i]['Actors']
+            awards = data_movie[i]['Awards']
+            country = data_movie[i]['Country']
+            director = data_movie[i]['Director']
+            genre = data_movie[i]['Genre']
+            imdbVotes = data_movie[i]['imdbVotes']
+            language = data_movie[i]['Language']
+            plot = data_movie[i]['Plot']
+            poster = data_movie[i]['Poster']
+            rated = data_movie[i]['Rated']
+            ratings = data_movie[i]['Ratings']
+            released = data_movie[i]['Released']
+            runtime = data_movie[i]['Runtime']
+            title = data_movie[i]['Title']
+            type = data_movie[i]['Type']
+            writer = data_movie[i]['Writer']
+            year = data_movie[i]['Year']
+            result["Actors"] = actors
+            result["Awards"] = awards
+            result["Country"] = country
+            result["Director"] = director
+            result["Genre"] = genre
+            result["imdbVotes"] = imdbVotes
+            result["Language"] = language
+            result["Plot"] = plot
+            result["Poster"] = poster
+            result["Rated"] = rated
+            result["Ratings"] = ratings
+            result["Released"] = released
+            result["Runtime"] = runtime
+            result["Title"] = title
+            result["Type"] = type
+            result["Writer"] = writer
+            result["Year"] = year
+
+    for i in result:
+        url_result = 'https://dsci551-a1e31.firebaseio.com/Movie_info_result.json'
+        data_movie = json.dumps(result)
+        response = requests.put(url_result,data_movie)
